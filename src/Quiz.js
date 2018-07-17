c01eed12e5dc5cb34659a8c09f15373144c2f784
Kimberly Williamson <kwilliamson@uen.org>
2018-07-17 13:52:07 -0600

    5

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,12 @@ class Quiz extends Component{
         this.state = {quiz_position: 1};
     }
 
+    showNextQuestion() {
+        this.setState((prevState)=> {
+            return {quiz_position: prevState.quiz_position + 1};
+        });
+    }
+
     render() {
         const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length;
 
@@ -18,7 +24,8 @@ class Quiz extends Component{
         <div>
             {isQuizEnd ?
                 <QuizEnd/> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                              showNextQuestionHandler={this.showNextQuestion.bind(this)}/>
             }
         </div>
         )
